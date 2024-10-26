e7df5c165bfdb9569f6e8881590e979ef52e2ba2
don-yerdna <don.yerdna@gmail.com>
2024-10-26 13:18:40 +0300

    start

---
+++ src/main/java/springmvc/config/AppInit.java
@@ -0,0 +1,22 @@
+package springmvc.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.swing.*;
+
+public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{AppConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
